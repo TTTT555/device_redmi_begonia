3ec16848e8c5f7112c5990532d9abbbc4a823b6d
TTTT555 <albukerko@gmail.com>
2021-02-18 12:12:05 +0000

    begonia: Init: Upd fingerprint (feb)

--- lineage_begonia.mk
+++ lineage_begonia.mk
@@ -43,13 +43,13 @@ PRODUCT_BRAND := Redmi
 PRODUCT_MANUFACTURER := Xiaomi
 
 # Fingerprint
-BUILD_FINGERPRINT := "google/coral/coral:11/RQ1A.210105.003/7005429:user/release-keys"
+BUILD_FINGERPRINT := "google/coral/coral:11/RQ1A.210205.004/7038034:user/release-keys"
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.vendor.build.fingerprint=$(BUILD_FINGERPRINT)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="coral-user 11 RQ1A.210105.003 7005429 release-keys" \
+    PRIVATE_BUILD_DESC="coral-user 11 RQ1A.210205.004 7038034 release-keys" \
     PRODUCT_NAME="begonia" \
     TARGET_DEVICE="begonia"
 
