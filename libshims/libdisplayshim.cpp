e5d8a974e152aa0c0c73b3fda1687e6632e97f09
TTTT555 <albukerko@gmail.com>
2021-03-12 15:19:23 +0000

    prep shim

---
+++ libshims/libdisplayshim.cpp
@@ -0,0 +1,16 @@
+#include <stdint.h>
+
+namespace android {
+    // libandroid_runtime.so
+    extern "C" void _ZN6vendor6xiaomi8hardware14displayfeature4V1_015IDisplayFeature10getServiceERKNSt3__112basic_stringIcNS5_11char_traitsIcEENS5_9allocatorIcEEEEb() {}
+    extern "C" void _ZN6vendor6xiaomi8hardware14displayfeature4V1_023IDisplayFeatureCallback11linkToDeathERKN7android2spINS5_8hardware20hidl_death_recipientEEEm() {}
+    extern "C" void _ZN6vendor6xiaomi8hardware14displayfeature4V1_023IDisplayFeatureCallback12getDebugInfoENSt3__18functionIFvRKN7android4hidl4base4V1_09DebugInfoEEEE() {}
+    extern "C" void _ZN6vendor6xiaomi8hardware14displayfeature4V1_023IDisplayFeatureCallback12getHashChainENSt3__18functionIFvRKN7android8hardware8hidl_vecINS8_10hidl_arrayIhLm32EJEEEEEEEE() {}
+    extern "C" void _ZN6vendor6xiaomi8hardware14displayfeature4V1_023IDisplayFeatureCallback13unlinkToDeathERKN7android2spINS5_8hardware20hidl_death_recipientEEE() {}
+    extern "C" void _ZN6vendor6xiaomi8hardware14displayfeature4V1_023IDisplayFeatureCallback14interfaceChainENSt3__18functionIFvRKN7android8hardware8hidl_vecINS8_11hidl_stringEEEEEE() {}
+    extern "C" void _ZN6vendor6xiaomi8hardware14displayfeature4V1_023IDisplayFeatureCallback19interfaceDescriptorENSt3__18functionIFvRKN7android8hardware11hidl_stringEEEE() {}
+    extern "C" void _ZN6vendor6xiaomi8hardware14displayfeature4V1_023IDisplayFeatureCallback21notifySyspropsChangedEv() {}
+    extern "C" void _ZN6vendor6xiaomi8hardware14displayfeature4V1_023IDisplayFeatureCallback21setHALInstrumentationEv() {}
+    extern "C" void _ZN6vendor6xiaomi8hardware14displayfeature4V1_023IDisplayFeatureCallback4pingEv() {}
+    extern "C" void _ZN6vendor6xiaomi8hardware14displayfeature4V1_023IDisplayFeatureCallback5debugERKN7android8hardware11hidl_handleERKNS6_8hidl_vecINS6_11hidl_stringEEE() {}
+}
